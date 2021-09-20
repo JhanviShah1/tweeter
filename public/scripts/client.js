76dea2ff0c24c40637d175529ef1cd31b13be497
JhanviShah1 <jhanvi.213@gmail.com>
2021-09-20 14:33:23 -0400

    Added validation errors withjquery

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,118 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+$(document).ready(() => {
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+  const data = [
+    {
+      user: {
+        name: "Newton",
+        avatars: "https://i.imgur.com/73hZDYK.png",
+        handle: "@SirIsaac",
+      },
+      content: {
+        text: "If I have seen further it is by standing on the shoulders of giants",
+      },
+      created_at: 1461116232227,
+    },
+    {
+      user: {
+        name: "Descartes",
+        avatars: "https://i.imgur.com/nlhLi3I.png",
+        handle: "@rd",
+      },
+      content: {
+        text: "Je pense , donc je suis",
+      },
+      created_at: 1461113959088,
+    },
+  ];
+  const createTweetElement = function (tweet) {
+    let $tweet = `
+  <article class = "tweet">
+          <header class="subheader">
+            <div class="imgtext">
+              <img src="${tweet.user.avatars}">
+           <p> ${tweet.user.name} </p>
+            </div> 
+          <div>
+          ${tweet.user.handle}
+          </div>
+        </header>
+        <div class="content">${escape(tweet.content.text)}
+        </div>
+          <footer class="footer">
+            <div class="timeStamp">
+            ${tweet.created_at}
+            </div>
+            <div>
+              <i class="fa fa-flag"></i><i class="fa fa-retweet"></i> <i class="fa fa-heart"></i>
+            </div>
+          </footer>
+        </article>
+  `;
+    return $tweet;
+  };
 
+  const renderTweets = function (tweets) {
+    for (const tweet of tweets) {
+      // calls createTweetElement for each tweet
+      const formattedTweet = createTweetElement(tweet);
+      // takes return value and appends it to the tweets container
+      $("#tweets-container").prepend(formattedTweet);
+    }
+  };
+  //renderTweets(data);
+  const handleError = function (error) {
+    if (error === "empty") {
+      $(".error-message").empty();
+      $(".error-message")
+        .append(
+          '<p><i class="fas fa-exclamation-triangle"></i>Input cannot be empty<i class="fas fa-exclamation-triangle"></i></p>'
+        )
+        .slideDown(1000);
+    } else if (error === "maxChar") {
+      $(".error-message").empty();
+      $(".error-message")
+        .append(
+          '<p><i class="fas fa-exclamation-triangle"></i>"Too long. Pls rspct our arbitary limit of 140 chars"<i class="fas fa-exclamation-triangle"></i></p>'
+        )
+        .slideDown(2000);
+    }
+  };
+  const $form = $("#createTweetForm");
+  $form.on("submit", function (event) {
+    event.preventDefault();
+    const input = $("#tweet-text").val();
+    if (input.length <= 0) {
+      handleError("empty");
+    }
+    if (input.length > 140) {
+      // alert("the input cannot be more than 140 characters")
+      handleError("maxChar");
+    }
+    const serializeData = $(this).serialize();
+    $.ajax({
+      method: "POST",
+      url: "/tweets",
+      data: serializeData,
+    }).done(function (msg) {
+      loadTweets();
+    });
+  });
+  const loadTweets = function () {
+    $.ajax({
+      method: "GET",
+      url: "/tweets",
+      data: JSON,
+    }).then(function (data) {
+      $("#tweets-container").empty();
+      renderTweets(data);
+    });
+  };
+  loadTweets();
+});
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,15 +1,21 @@
 
 $(document).ready(function() {
 $("#btn").on('click',function(){
-  console.log(this);
+  //console.log(this);
 });//button 
+const $article = $('article footer .timeStamp')
+const timeAgo = timeago.format(new Date());
+$article.text(timeAgo);
+
 $("#tweet-text").on('input',function(){
   const input = $(this).val()
   const remaining = 140-input.length;
   $(".counter").val(remaining);
   if (remaining<0) {
   $(".counter").css("color","red");
-alert("The text is too long")}
+//alert("The text is too long")
+}
 
 })
 });
+
